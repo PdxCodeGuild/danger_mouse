ee397199bb382c7c850756a6e4d834d73a364319
JennaMTucker <jenna.m.tucker@gmail.com>
2017-06-06 15:38:09 -0700

    "June 6, 2017 3:38pm"

--- mouse_test.py
+++ mouse_test.py
@@ -5,14 +5,15 @@ from item import Food
 
 
 kitchen = Room("kitchen", "The kitchen", "kitchen", [])
-#should have instantiated a room inventory
+# should have instantiated a room inventory
 mouse = Mouse("Horace", "A wise, old mouse.", kitchen)
 #should have instantiated a character inventory
 cheese = Food("cheese", 20)
-
+#
 print(kitchen.characters)
 kitchen.characters.append(Rat("a rat", []))
 print(kitchen.characters)
+#
 kitchen.inventory.put_in(cheese)
 print(kitchen.inventory)
 
