f0fe76c39fbc5260f7327f7d7cd795ab04cd16f8
Conner Ramirez <connerramirez@gmail.com>
2017-06-07 14:16:55 -0700

    adf

--- game_menu.py
+++ game_menu.py
@@ -31,7 +31,7 @@ while not game_over:
                           '2. Move \n'
                           '3. Peek through a door \n'
                           '4. Inventory \n'
-                          '5. Interact')).lower()
+                          '5. Interact\n')).lower()
 
     if '1' in action_select or 'look' in action_select:
         i = 1
@@ -74,7 +74,7 @@ while not game_over:
         item_action = input('What do you wish to do with this item? \n'
                             '1. Look at item \n'
                             '2. Use item \n'
-                            '3. Drop item')
+                            '3. Drop item\n')
         if '1' in action_select:
             inv_dict[item_select].look()
             print('\n')
@@ -96,7 +96,7 @@ while not game_over:
             action_dict[str(i)] = character
             i += 1
         pretty_print_dict(action_dict)
-        action_item = input('What do you want to interact with?')
+        action_item = input('What do you want to interact with?\n')
         action_dict[action_item].action(current_room, danger_mouse)
 
     else:
