5db79081319f22eb9d22af77690a4f4de239984e
JennaMTucker <jenna.m.tucker@gmail.com>
2017-06-07 10:36:08 -0700

    June 7 10:36am

---
+++ character_profile.py
@@ -0,0 +1,43 @@
+
+while True:
+    print("""
+        Welcome to the game Danger Mouse!  
+        Your goal will be to avoid danger
+        while gathering enough food from
+        rooms in the castle to last a day.
+        
+        You will find various spells to 
+        aid you.  Keep an eye on your health, 
+        as you will need to eat throughout 
+        the day and also store food to bring 
+        home.  
+          
+        Please choose a character to play:
+        
+        1. Mortimer - a wise mouse with a
+        keen understanding of the dogs
+        and people who occupy the castle.  
+        
+        2. Sydney - a clever mouse skilled
+        at hiding and evasion from the rats,
+        cats, dogs, and people who occupy
+        the castle.  
+        
+        3. Aster - a brave mouse quick to
+        cause fright in rats and people
+        who occupy the castle.  
+        
+        """)
+
+    try:
+        player = input("Please choose your mouse: ")
+
+    except:
+
+    if player == "1":
+
+    elif player == "2":
+
+    elif player == "3":
+
+
