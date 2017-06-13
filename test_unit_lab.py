6c382cba9669c702ecb0c6537ac14c06cd5c2c5d
JennaMTucker <jenna.m.tucker@gmail.com>
2017-06-13 11:07:44 -0700

    June 13 11:07am

--- test_unit_lab.py
+++ test_unit_lab.py
@@ -2,8 +2,8 @@ import unittest
 from character import Dog
 
 class CharacterTest(unittest.TestCase):
-    def setup(self):
-        self.a_dog = Dog()
+    def setUp(self):
+        self.a_dog = Dog("kitchen")
 
     def testCharacter(self):
         self.assertEqual(self.a_dog.name, "dog")
@@ -11,4 +11,9 @@ class CharacterTest(unittest.TestCase):
         self.assertEqual(self.a_dog.friend, False)
         self.assertEqual(self.a_dog.resting, False)
         self.assertEqual(self.a_dog.searching, 0)
-        self.assertTrue(1 >= self.aggression >= 4)
+        self.assertTrue(1 <= self.a_dog.aggression <= 4)
+        self.a_dog.activate()
+        
+
+if __name__ == '__main__':
+    unittest.main()
--- unit_testing.py
+++ unit_testing.py
@@ -19,16 +19,17 @@ class CharacterTest(unittest.TestCase):
 
 class CharacterTest(unittest.TestCase):
     def setUp(self):
-        self.food.item = Food("wedge")
+        self.food_item = Food("wedge")
         self.player = Mouse("Chris", "Description", "location")
         self.player.take_item(self.food_item)
 
     def testCharacter(self):
         self.assertEqual(self.player.name, "Chris")
-        self.assertEqual(self.player.description, "sdf")
+        # self.assertEqual(self.player.description, "sdf")
         self.assertEqual(self.player.health, 100)
         self.player.inventory.bag_of_holding[0].eat(self.player)
         self.assertEqual(self.player.health, 105)
+
 
 # lab, pick a class that we"ve worked on in the test game and run tests for it
 # you might have to fake an input, there is a way to do it
